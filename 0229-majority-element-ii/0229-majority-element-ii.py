87d131cbdbc6515523e6b6199d4f9d91cedc75bf
Nathnael Teketel Agafari <nathiteketel1@gmail.com>
2024-02-10 17:15:51 +0300

    Time: 94 ms (87.65%), Space: 17.9 MB (61.38%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,23 +1,9 @@
 class Solution:
-  def majorityElement(self, nums: List[int]) -> List[int]:
-    ans1 = 0
-    ans2 = 1
-    count1 = 0
-    count2 = 0
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        n = len(nums) // 3
 
-    for num in nums:
-      if num == ans1:
-        count1 += 1
-      elif num == ans2:
-        count2 += 1
-      elif count1 == 0:
-        ans1 = num
-        count1 = 1
-      elif count2 == 0:
-        ans2 = num
-        count2 = 1
-      else:
-        count1 -= 1
-        count2 -= 1
+        nums_freq_map = {}
+        for num in nums:
+            nums_freq_map[num] = nums_freq_map.get(num, 0) + 1
 
-    return [ans for ans in (ans1, ans2) if nums.count(ans) > len(nums) // 3]
+        return [x for x, y in nums_freq_map.items() if y > n]
