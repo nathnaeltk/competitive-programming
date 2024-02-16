04658cfe8c77e7bb721d36edd10ae3adba3647f7
Nathnael Teketel Agafari <nathiteketel1@gmail.com>
2024-02-16 18:15:44 +0300

    Time: 58 ms (75.54%), Space: 17.9 MB (58.04%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,18 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # Create a dictionary to store the indices of numbers as we iterate through the array
         num_indices = {}
 
         for i, num in enumerate(nums):
             complement = target - num
 
-            # Check if the complement is in the dictionary
             if complement in num_indices:
-                # Return the indices of the two numbers that add up to the target
                 return [num_indices[complement], i]
             
-            # Store the current number's index in the dictionary
             num_indices[num] = i
 
-        # If no solution is found
         return []
