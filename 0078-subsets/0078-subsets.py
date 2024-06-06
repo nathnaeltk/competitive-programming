b9ae630ee24d316ce24908e32b5aca6f6cfc74af
Nathnael Teketel Agafari <nathiteketel1@gmail.com>
2024-06-06 20:56:58 +0300

    Time: 44 ms (11.99%), Space: 16.7 MB (29.27%) - LeetHub

---
+++ 0078-subsets/0078-subsets.py
@@ -0,0 +1,18 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        
+        subset = []
+        def findSubsets(i):
+            if i >= len(nums):
+                result.append(subset.copy())
+                return
+            
+            subset.append(nums[i])
+            findSubsets(i + 1)
+            
+            subset.pop()
+            findSubsets(i + 1)
+            
+        findSubsets(0)
+        return result
