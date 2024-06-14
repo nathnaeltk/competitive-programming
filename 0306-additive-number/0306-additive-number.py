27dd797c2c8aa54e51e21eec8734e9f3c3d61c88
Nathnael Teketel Agafari <nathiteketel1@gmail.com>
2024-06-14 22:22:02 +0300

    Time: 37 ms (45.65%), Space: 16.5 MB (71.76%) - LeetHub

---
+++ 0306-additive-number/0306-additive-number.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isAdditiveNumber(self, num: str) -> bool:
+        def check_sequence(a: int, b: int, remaining: str) -> bool:
+            if not remaining:
+                return True
+            
+            c = a + b
+            c_str = str(c)
+            if remaining.startswith(c_str):
+                return check_sequence(b, c, remaining[len(c_str):])
+            
+            return False
+        
+        length = len(num)
+        
+        for first_end in range(1, length):
+            if first_end > 1 and num[0] == '0':
+                break
+            
+            for second_end in range(first_end + 1, length):
+                if second_end - first_end > 1 and num[first_end] == '0':
+                    break
+                
+                first_num = int(num[:first_end])
+                second_num = int(num[first_end:second_end])
+                
+                if check_sequence(first_num, second_num, num[second_end:]):
+                    return True
+        
+        return False
