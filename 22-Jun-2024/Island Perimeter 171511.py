5c84239f1fb185c8e72aa0fda6f5b070d8c43f25
Nathnael Teketel Agafari <nathiteketel1@gmail.com>
2024-06-22 20:02:17 +0300

    Created 'Island Perimeter' by A2SV Hub

---
+++ 22-Jun-2024/Island Perimeter 171511.py
@@ -0,0 +1,24 @@
+# Problem: Island Perimeter - https://leetcode.com/problems/island-perimeter/description/
+
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        visited = set()
+        
+        def dfs(i, j):
+            if i >= len(grid) or j >= len(grid[0]) or i < 0 or j < 0 or grid[i][j] == 0:
+                return 0
+            if (i, j) in visited:
+                return 1
+            
+            visited.add((i, j))
+            perimeter = dfs(i, j + 1)
+            perimeter += dfs(i+1, j)
+            perimeter += dfs(i - 1, j)
+            perimeter += dfs(i, j + 1)
+            perimeter += dfs(i, j - 1)
+            
+            return perimeter
+        
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                return dfs(i, j)
