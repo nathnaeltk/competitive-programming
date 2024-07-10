2f771d75f83c7ff8684bde77b2c63e3d2a1e447c
Nathnael Teketel Agafari <nathiteketel1@gmail.com>
2024-07-10 21:51:17 +0300

    Time: 41 ms (22.53%), Space: 16.7 MB (10.97%) - LeetHub

---
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -0,0 +1,30 @@
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        adj = defaultdict(list)
+        
+        # Building the graph
+        for i, (a, b) in enumerate(equations):
+            adj[a].append((b, values[i]))
+            adj[b].append((a, 1 / values[i]))
+        
+        def bfs(source: str, target: str) -> float:
+            if source not in adj or target not in adj:
+                return -1.0
+            
+            q = deque([(source, 1.0)])
+            visit = set([source])
+            
+            while q:
+                n, w = q.popleft()
+                
+                if n == target:
+                    return w
+                
+                for neighbor, weight in adj[n]:
+                    if neighbor not in visit:
+                        q.append((neighbor, w * weight))
+                        visit.add(neighbor)
+            
+            return -1.0
+        
+        return [bfs(q[0], q[1]) for q in queries]
