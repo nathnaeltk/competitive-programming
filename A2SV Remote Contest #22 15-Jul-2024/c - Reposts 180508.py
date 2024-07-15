101d9a9b53be4e6751ebd68b0ebb1694a5f57baf
Nathnael Teketel Agafari <nathiteketel1@gmail.com>
2024-07-15 17:01:45 +0300

    Created 'c - Reposts' by A2SV Hub

---
+++ A2SV Remote Contest #22 15-Jul-2024/c - Reposts 180508.py
@@ -0,0 +1,23 @@
+# Problem: c - Reposts - https://codeforces.com/gym/533316/problem/c
+
+class Solution:
+
+	def solve(self, reposts, n):
+		lookup = {'polycarp': 1}
+
+		for repost in reposts:
+			name1, _, name2 = repost.split(" ")
+			lookup[name1] = 1 + lookup[name2]
+		print(max(lookup.values()))
+		return
+
+
+t = 1
+sol = Solution()
+while t:
+    n = int(input())
+    reposts = []
+    for _ in range(n):
+        reposts.append(input().lower())
+    sol.solve(reposts, n)
+    t -= 1
