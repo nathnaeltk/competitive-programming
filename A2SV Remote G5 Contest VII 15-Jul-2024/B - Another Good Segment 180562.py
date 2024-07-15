a06961197a10af07d31d5f6d46b8e47222863905
Nathnael Teketel Agafari <nathiteketel1@gmail.com>
2024-07-15 20:59:41 +0300

    Created 'B - Another Good Segment' by A2SV Hub

---
+++ A2SV Remote G5 Contest VII 15-Jul-2024/B - Another Good Segment 180562.py
@@ -0,0 +1,20 @@
+# Problem: B - Another Good Segment - https://codeforces.com/gym/511771/problem/B
+
+n, s = map(int, input().split())
+
+arr = list(map(int, input().split()))
+freq = {}
+left = 0
+count = 0
+
+for right in range(n):
+    freq[arr[right]] = freq.get(arr[right], 0) + 1
+
+    while len(freq) > s:
+        freq[arr[left]] -= 1
+        if freq[arr[left]] == 0:
+            del freq[arr[left]]
+        left += 1
+
+    count += right-left+1
+print(count) 
