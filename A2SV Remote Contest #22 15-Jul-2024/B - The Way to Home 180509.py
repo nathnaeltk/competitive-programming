b03da11dad6f7021acd75027aed09d549d0dea05
Nathnael Teketel Agafari <nathiteketel1@gmail.com>
2024-07-15 17:01:51 +0300

    Created 'B - The Way to Home' by A2SV Hub

---
+++ A2SV Remote Contest #22 15-Jul-2024/B - The Way to Home 180509.py
@@ -0,0 +1,26 @@
+# Problem: B - The Way to Home - https://codeforces.com/gym/533316/problem/B
+
+dp = [0]
+n, d = map(int, input().split())
+s = input()
+
+for i in range(1, len(s)):
+
+    if s[i] == '0':
+        dp.append(10 ** 10)
+    else:
+
+        mn = 10 ** 10
+        for j in range(max(0, i - d), i):
+            mn = min(mn, dp[j])
+        if mn == 10 ** 10:
+            dp.append(10 ** 10)
+        else:    
+            dp.append(mn + 1)
+abc = 1
+if dp[-1] == 10 ** 10:
+    print(-1)
+else:
+    print(dp[-1])
+
+    
