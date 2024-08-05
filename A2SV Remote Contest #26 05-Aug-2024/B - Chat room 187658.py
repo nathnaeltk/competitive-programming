4a0139b7ace02a590a69bd724d0e633882216b66
Nathnael Teketel Agafari <nathiteketel1@gmail.com>
2024-08-05 22:04:55 +0300

    Created 'B - Chat room' by A2SV Hub

---
+++ A2SV Remote Contest #26 05-Aug-2024/B - Chat room 187658.py
@@ -0,0 +1,14 @@
+# Problem: B - Chat room - https://codeforces.com/gym/540348/problem/B
+
+s = input().strip()
+target = "hello"
+j = 0
+
+for i in range(len(s)):
+    if s[i] == target[j]:
+        j += 1
+    if j == len(target):
+        print("YES")
+        break
+else:
+    print("NO")
