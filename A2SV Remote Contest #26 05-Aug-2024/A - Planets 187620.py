5c319d79adfe567b2f39136c395c362259e0f2af
Nathnael Teketel Agafari <nathiteketel1@gmail.com>
2024-08-05 21:19:21 +0300

    Created 'A - Planets' by A2SV Hub

---
+++ A2SV Remote Contest #26 05-Aug-2024/A - Planets 187620.py
@@ -0,0 +1,20 @@
+# Problem: A - Planets - https://codeforces.com/gym/540348/problem/A
+
+t = int(input())
+
+for _ in range(t):
+    n, c = map(int, input().split())
+    orbits = list(map(int, input().split()))
+    
+    freq = {}
+    for orbit in orbits:
+        if orbit in freq:
+            freq[orbit] += 1
+        else:
+            freq[orbit] = 1
+    
+    min_cost = 0
+    for orbit, count in freq.items():
+        min_cost += min(count, c)
+    
+    print(min_cost)
