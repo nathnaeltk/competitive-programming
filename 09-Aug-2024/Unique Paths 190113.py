4b22db7fd1503bc851c4296a27d0b2bbfed63004
Nathnael Teketel Agafari <nathiteketel1@gmail.com>
2024-08-09 22:00:34 +0300

    Created 'Unique Paths' by A2SV Hub

---
+++ 09-Aug-2024/Unique Paths 190113.py
@@ -0,0 +1,18 @@
+# Problem: Unique Paths - https://leetcode.com/problems/unique-paths/
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        memo = {} # coordinates: how many ways to get to that coordinate
+
+        def dp(x, y):
+            if x > m or y > n:
+                return 0
+            if (x, y) == (m-1, n-1):
+                return 1
+            if (x, y) in memo:
+                return memo[(x, y)]
+
+            memo[(x, y)] = dp(x+1, y) + dp(x, y+1)
+            return memo[(x, y)]
+
+        return dp(0, 0)
