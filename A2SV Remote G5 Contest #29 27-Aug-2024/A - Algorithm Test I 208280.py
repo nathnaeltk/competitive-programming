e19faa686c68e7069f189d742ba845d7052d4ee5
Nathnael Teketel Agafari <nathiteketel1@gmail.com>
2024-08-27 14:08:10 +0300

    Created 'A - Algorithm Test I' by A2SV Hub

---
+++ A2SV Remote G5 Contest #29 27-Aug-2024/A - Algorithm Test I 208280.py
@@ -0,0 +1,12 @@
+# Problem: A - Algorithm Test I - https://codeforces.com/gym/544347/problem/A
+
+import math
+
+m = int(input())  
+a = list(map(int, input().split()))  
+
+distinct_colors = set(a)
+
+result = math.factorial(len(distinct_colors))
+
+print(result)
