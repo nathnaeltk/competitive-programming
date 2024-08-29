c0d442ae65a792ac7d9d6295fb9fd33f5a8f1490
Nathnael Teketel Agafari <nathiteketel1@gmail.com>
2024-08-29 15:12:55 +0300

    Created 'Missing Number' by A2SV Hub

---
+++ 29-Aug-2024/Missing Number 208941.py
@@ -0,0 +1,8 @@
+# Problem: Missing Number - https://leetcode.com/problems/missing-number/description/
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        expected_sum = (n * (n + 1)) // 2 
+        actual_sum = sum(nums)
+        return expected_sum - actual_sum
