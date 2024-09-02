0a0f4f70713aad255155b25cc5dad4df4da2d2dd
Nathnael Teketel Agafari <nathiteketel1@gmail.com>
2024-09-02 21:58:49 +0300

    Created 'B - Amro and Bits' by A2SV Hub

---
+++ A2SV Remote Contest #30 02-Sep-2024/B - Amro and Bits 210485.py
@@ -0,0 +1,28 @@
+# Problem: B - Amro and Bits - https://codeforces.com/gym/545837/problem/B
+
+t = int(input())  
+for _ in range(t):
+    x = int(input()) 
+    binary_x = bin(x) 
+    first_one_index = 0
+
+    for i in range(len(binary_x) - 1, 1, -1):
+        if binary_x[i] == '1':
+            first_one_index = i
+            break
+
+    result_bin = '0b' + binary_x[first_one_index:] 
+    other_one_found = False
+
+    for i in range(len(binary_x) - 1, 1, -1):
+        if binary_x[i] == '1' and i != first_one_index:
+            other_one_found = True
+            break
+
+    if not other_one_found:
+        if len(result_bin) == 3:
+            result_bin += '1'
+        else:
+            result_bin = result_bin[:-1] + '1'
+
+    print(int(result_bin, 2))  
