baa0d70823772f63118338457656fa56159b0ff1
Nathnael Teketel Agafari <nathiteketel1@gmail.com>
2024-09-08 23:34:55 +0300

    Created 'Number of Provinces' by A2SV Hub

---
+++ 08-Sep-2024/Number of Provinces 213640.py
@@ -0,0 +1,9 @@
+# Problem: Number of Provinces - https://leetcode.com/problems/number-of-provinces/
+
+class Solution:
+    def findCircleNum(self, is_connected: list[list[int]]) -> int:
+        def visit_from(x: int, seen: set[int]) -> bool:
+            return seen.add(x) or all(visit_from(y, seen) for y, c in enumerate(is_connected[x]) if y not in seen and c)
+
+        seen = set()
+        return sum((x not in seen) and visit_from(x, seen) for x in range(len(is_connected)))
