2fb0052e9c9b55c6fea179c7e8ad90fd41ea5f71
Nathnael Teketel Agafari <nathiteketel1@gmail.com>
2024-09-08 23:36:18 +0300

    Created 'A - Duale String' by A2SV Hub

---
+++ A2SV Remote G5 Contest #31 08-Sep-2024/A - Duale String 213641.py
@@ -0,0 +1,20 @@
+# Problem: A - Duale String - https://codeforces.com/gym/547609/problem/A
+
+def is_duale(s):
+    if len(s) % 2 != 0:
+        return "NO"
+    else:
+        mid = len(s) // 2
+        if s[:mid] == s[mid:]:
+            return "YES"
+        else:
+            return "NO"
+
+def main():
+    t = int(input())
+    for _ in range(t):
+        s = input().strip()
+        print(is_duale(s))
+
+if __name__ == "__main__":
+    main()
