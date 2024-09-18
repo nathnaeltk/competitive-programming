f647275814174cfd5416b66d4a5f8263d9bdfa50
Nathnael Teketel Agafari <nathiteketel1@gmail.com>
2024-09-18 05:42:30 +0300

    Created 'Cirno's Perfect Bitmasks Classroom' by A2SV Hub

---
+++ 18-Sep-2024/Cirno's Perfect Bitmasks Classroom 215704.py
@@ -0,0 +1,9 @@
+# Problem: Cirno's Perfect Bitmasks Classroom - https://codeforces.com/problemset/problem/1688/A
+
+t=int(input())
+for w in range(t):
+	x=int(input())
+	y=x&-x
+	while x==y or x&y==0:
+		y+=1
+	print(y)
