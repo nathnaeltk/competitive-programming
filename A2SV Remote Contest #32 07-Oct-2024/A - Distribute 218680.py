a63d4dbae1afaef76421974639d56727eb6c4298
Nathnael Teketel Agafari <nathiteketel1@gmail.com>
2024-10-07 21:12:47 +0300

    Created 'A - Distribute' by A2SV Hub

---
+++ A2SV Remote Contest #32 07-Oct-2024/A - Distribute 218680.py
@@ -0,0 +1,15 @@
+# Problem: A - Distribute - https://codeforces.com/gym/555625/problem/A
+
+n = int(input())
+cards = list(map(int, input().split()))
+dict = {}
+
+for i, card in enumerate(cards):
+    if card in dict:
+        dict[card].append(i + 1)
+    else:
+        dict[card] = [i + 1]
+cards.sort()
+
+for i in range(n // 2):
+    print(dict[cards[i]].pop(0), dict[cards[n - 1 - i]].pop(0)) 
