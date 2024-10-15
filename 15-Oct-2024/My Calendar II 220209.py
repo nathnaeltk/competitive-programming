a99992bc387bd612f4c68c5102b10ac631534712
Nathnael Teketel Agafari <nathiteketel1@gmail.com>
2024-10-15 22:46:22 +0300

    Created 'My Calendar II' by A2SV Hub

---
+++ 15-Oct-2024/My Calendar II 220209.py
@@ -0,0 +1,24 @@
+# Problem: My Calendar II - https://leetcode.com/problems/my-calendar-ii/
+
+from sortedcontainers import SortedList
+class MyCalendarTwo:
+
+    def __init__(self):
+        self.calendar = SortedList()
+
+    def book(self, start: int, end: int) -> bool:
+        stuple, etuple = (start, 1), (end, -1)
+        self.calendar.add(stuple)
+        self.calendar.add(etuple)
+        total = 0
+        for _, count in self.calendar:
+            total += count
+            if total == 3:
+                self.calendar.remove(stuple)
+                self.calendar.remove(etuple)
+                return False
+        return True
+        
+# Your MyCalendarTwo object will be instantiated and called as such:
+# obj = MyCalendarTwo()
+# param_1 = obj.book(start,end)
