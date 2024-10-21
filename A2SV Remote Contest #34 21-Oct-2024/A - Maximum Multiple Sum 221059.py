3b583c46bb025cdd803e791e644b18cda7443cd6
Nathnael Teketel Agafari <nathiteketel1@gmail.com>
2024-10-21 16:21:07 +0300

    Created 'A - Maximum Multiple Sum' by A2SV Hub

---
+++ A2SV Remote Contest #34 21-Oct-2024/A - Maximum Multiple Sum 221059.py
@@ -0,0 +1,18 @@
+# Problem: A - Maximum Multiple Sum - https://codeforces.com/gym/559568/problem/A
+
+# Input
+t = int(input())  # number of test cases
+test_cases = [int(input()) for _ in range(t)]
+
+# Process each test case
+results = []
+for n in test_cases:
+    max_sum = 0
+    best_x = 2
+    for x in range(2, n + 1):
+        k = n // x
+        current_sum = x * k * (k + 1) // 2
+        if current_sum > max_sum:
+            max_sum = current_sum
+            best_x = x
+    print(best_x)
